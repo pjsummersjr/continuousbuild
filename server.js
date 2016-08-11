d6695432b5d929023e7b67a219d4d912f9b41def
Paul <paul.summers@microsoft.com>
2016-08-10 22:57:27 -0400

    More app settings stuff

--- server.js
+++ server.js
@@ -4,4 +4,5 @@ app.set('port', process.env.PORT || 3000);
 
 var server = app.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + server.address().port);
+  console.log('API Key: ' + process.env.APP_KEY);
 });
